f896334bae8357034b51a432215134c3a5192327
kiranreddi5472 <kiranreddi54726@gmail.com>
2026-05-22 17:27:24 +0530

    Configure STATIC_ROOT for PythonAnywhere deployment

--- birthday_backend/settings.py
+++ birthday_backend/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Media files (User uploaded images for wishes & memories)
 MEDIA_URL = '/media/'
